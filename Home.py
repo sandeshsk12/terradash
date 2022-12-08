d89010db1f6ba689e44a3c447cec7e500ebe0f36
sandeshsk12 <sksandeshkandagal@gmail.com>
2022-12-08 09:24:11 +0530

    final -1

--- Home.py
+++ Home.py
@@ -94,6 +94,27 @@ st.markdown(
         e. Wallets age
     """
 )
+st.markdown(
+    """
+    # Conclusion
+
+    1. The number of weekly transaction has been consistently hovering around 50k a week
+
+    2. The transaction fee has reduced considerable in the last few weeks, with it now being clost to 0.02 uluna. 
+
+    3. Each block consists of 1 or 2 transactions which implies that terra is not running at full capacity.
+
+    4. 6 secords or 6000 milliseconds is the average block time on terra
+
+    5. Around 90% of the transactions always go through.
+
+    6. Only 5% of the users are active uers while 95% are inactive
+
+    7. In the recent weeks, Terra onboards close to 500 new users every week which makes up about 5% of theusers. 
+
+    8. Around half of the wallets are more than 6 months old. These wre all created when the hard fork occured.
+    """
+)
 
 
 
